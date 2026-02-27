16c137485c671f2df0b820461dcfecbd5bd53b37
Petri Huovinen <petri.huovinen@gmail.com>
2026-02-27 15:30:15 +0200

    adjust pipe gap

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ canvas.height = 900;
 const GRAVITY = 0.15;
 const FLAP_POWER = -5;
 const PIPE_WIDTH = 60;
-const PIPE_GAP = 120;
+const PIPE_GAP = 240;
 const PIPE_SPEED = -2.5;
 const PIPE_SPAWN_RATE = 90; // pixels between pipes
 
